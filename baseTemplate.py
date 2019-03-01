91f85f4e84535537638e155c4a5d90c0c0694cb7
RayNieva <raynieva@gmail.com>
2019-03-01 11:53:54 -0500

    added a Python coding template to increase efficiencies

---
+++ baseTemplate.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+#! python3
+
+
+# name.py - Describe here...
+
+import logging
+
+
+
+logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s- %(message)s')
+
+logging.debug('Start of program')
+
+
+
+def fun():
+    
+    # TODO: .....
+    pass
+    
+
+    
+def main():
+    fun()
+    logging.debug('End of program')
+
+
+if __name__ == '__main__':
+    main()
+
